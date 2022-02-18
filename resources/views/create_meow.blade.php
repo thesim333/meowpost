e0c23105f4b0d45f5ee0937b481227d830adacb5
sim <greengekko333@gmail.com>
2022-02-18 19:38:36 +1300

    Create page template to extend from

--- resources/views/create_meow.blade.php
+++ resources/views/create_meow.blade.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Cat Tails: Post New Meow</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <header class="my-4 text-center">
-            <h1>What do you want to Meow about today?</h1>
-        </header>
-        <main>
-            <form>
-                <div class="mb-3">
-                    <label for="meow-content" class="form-label">New Meow</label>
-                    <textarea id="meow-content" class="form-control" aria-label="New Meow" name="content"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Meow!</button>
-            </form>
-        </main>
-    </div>
-</body>
-</html>
+@extends('page')
+
+@section('title', 'Post New Meow')
+
+@section('heading', 'What do you want to Meow about today?')
+
+@section('content')
+    <main>
+        <form>
+            <div class="mb-3">
+                <label for="meow-content" class="form-label">New Meow</label>
+                <textarea id="meow-content" class="form-control" aria-label="New Meow" name="content"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Meow!</button>
+        </form>
+    </main>
+@endsection
