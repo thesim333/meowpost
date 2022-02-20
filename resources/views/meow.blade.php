e3835436582516f1c334567cd43d2e4d55ddaf75
sim <greengekko333@gmail.com>
2022-02-21 10:10:59 +1300

    extract meow to subview

---
+++ resources/views/meow.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <p>{{ $meow->content }}</p>
+    </div>
+    <div class="d-flex px-3 justify-content-between align-items-baseline">
+        <h6>User: {{ $meow->user_id }}</h6>
+        <span>{{ $meow->created_at->diffForHumans() }}</span>
+    </div>
+</div>
