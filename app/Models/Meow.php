b6bc79ec9ff81e9498fbc58cf76b14bd3b7c263f
Simon Winder <greengekko333@gmail.com>
2022-03-24 09:38:21 +1300

    with ide helper

--- app/Models/Meow.php
+++ app/Models/Meow.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Meow
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Meow whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Meow extends Model
 {
     use HasFactory;
--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Helpers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
