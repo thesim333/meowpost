66ff8023635a9cbf0d2d48dbd5f293afede96ca4
Simon Winder <greengekko333@gmail.com>
2022-03-25 12:36:02 +1300

    agree box is required and sends 'yes'

--- resources/views/terms-agree.blade.php
+++ resources/views/terms-agree.blade.php
@@ -13,7 +13,7 @@
             @csrf
             <div class="block mt-4">
                 <label for="agree" class="inline-flex items-center">
-                    <input id="agree" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="agree">
+                    <input id="agree" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="agree" value="yes" required>
                     <span class="ml-2 text-sm text-gray-600">{{ __('Agree') }}</span>
                 </label>
             </div>
