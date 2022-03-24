bdde9765b1a491a63acc373032abd6979b4429d7
Simon Winder <greengekko333@gmail.com>
2022-03-25 11:19:22 +1300

    link to register on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,8 +41,12 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="ml-3 underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
