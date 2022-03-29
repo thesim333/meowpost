561ad9d16f17d321a7bebff1c723406210424cef
Simon Winder <greengekko333@gmail.com>
2022-03-29 15:53:33 +1300

    Make Tag unique

--- database/migrations/2022_03_29_004032_create_tags_table.php
+++ database/migrations/2022_03_29_004032_create_tags_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('tag');
+            $table->string('tag')->unique();
             $table->timestamps();
         });
 
