b76ec3df304970a6d7de0e9398165acabec97c97
Simon Winder <greengekko333@gmail.com>
2022-04-05 12:08:09 +1200

    Clear tags link

--- resources/views/components/tag-links.blade.php
+++ resources/views/components/tag-links.blade.php
@@ -2,4 +2,8 @@
     @foreach ($tags as $tag)
         <x-tag-link :tag="$tag" />
     @endforeach
+    <div
+        class="inline-flex items-center px-4 py-2 bg-blue-600 border rounded-md font-semibold text-xs text-white uppercase tracking-widest">
+        <a href="{{ route('meows') }}">{{ __('Clear X') }}</a>
+    </div>
 </div>
